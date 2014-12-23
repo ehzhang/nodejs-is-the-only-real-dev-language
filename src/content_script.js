c36a6d9bf41a2fd73a7a87350076c8468643bc08
Edwin Zhang <ehzhang@mit.edu>
2014-12-23 03:07:42 -0500

    node js is the only real dev language

---
+++ src/content_script.js
@@ -0,0 +1,38 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+    textNode.nodeValue = v.split(" ").map(function(){return "node.js"}).join(" ")
+
+}
+
+
